945cfbf678d500d6f24c66a28193954f7a93980c
Toño Gonzalez <antoniogm14@gmail.com>
2026-06-11 10:16:19 -0500

    Revert: App.jsx a versión simple, elimina routing A/B, limpia CSS Hero B

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Analytics from './components/Analytics';
-import HeroA from './components/HeroA';
-import HeroB from './components/HeroB';
 import Hero from './components/Hero';
 import Somos from './components/Somos';
 import Planes from './components/Planes';
@@ -13,18 +11,11 @@ import Testimonios from './components/Testimonios';
 import CtaBand from './components/CtaBand';
 import Footer from './components/Footer';
 
-function getHero() {
-  const path = window.location.pathname;
-  if (path.startsWith('/b')) return <HeroB />;
-  if (path.startsWith('/a')) return <HeroA />;
-  return <Hero />;
-}
-
 export default function App() {
   return (
     <>
       <Analytics />
-      {getHero()}
+      <Hero />
       <Somos />
       <Planes />
       <Pasos />
