a3224ce8b544d4639e20ae795652826449e75071
Toño Gonzalez <antoniogm14@gmail.com>
2026-06-12 09:25:35 -0500

    Formaliza y Sunat: botones cambiados a Solicitar Asesoría → #contacto

--- src/components/Formaliza.jsx
+++ src/components/Formaliza.jsx
@@ -39,12 +39,7 @@ export default function Formaliza() {
               <li key={item}><CheckIcon />{item}</li>
             ))}
           </ul>
-          <a className="btn btn-coral formaliza-cta" href={waUrl} target="_blank" rel="noopener noreferrer">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M17.5 14.4c-.3-.1-1.6-.8-1.8-.9-.2-.1-.4-.1-.6.1-.2.3-.7.9-.8 1-.2.2-.3.2-.5.1-.3-.1-1.2-.4-2.2-1.3-.8-.7-1.4-1.6-1.5-1.9-.2-.3 0-.4.1-.6.1-.1.3-.3.4-.5.1-.2.2-.3.3-.5.1-.2 0-.4 0-.5-.1-.1-.6-1.5-.9-2.1-.2-.5-.4-.5-.6-.5h-.5c-.2 0-.5.1-.7.3-.3.3-1 1-1 2.4s1 2.8 1.2 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.6-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.1-.3-.2-.6-.3zM12 2C6.5 2 2 6.5 2 12c0 1.8.5 3.5 1.3 5L2 22l5.2-1.4C8.6 21.5 10.3 22 12 22c5.5 0 10-4.5 10-10S17.5 2 12 2z"/>
-            </svg>
-            Formalizar mi empresa
-          </a>
+          <a className="btn btn-coral formaliza-cta" href="#contacto">Solicitar Asesoría</a>
         </div>
       </div>
     </section>
